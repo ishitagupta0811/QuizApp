09d70a8a1cebbf699711ce55387867153b5bae29
Ishita Gupta <igupta@gitam.in>
2025-06-09 15:53:29 +0530

    timer to each question

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState }  from "react";
+
+export default function QuestionTimer({  timeout, onTimeOut }){
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    useEffect(() => {
+        console.log('SETTING TIMEOUT');
+        const timer = setTimeout(onTimeOut, timeout);
+
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [timeout, onTimeOut]);
+
+    useEffect(() => {
+        console.log('INTERVAL');
+        const interval = setInterval(() => {
+            setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+        }, 100);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
+
+    return (
+        <progress id = "question-time" value = {remainingTime} max = {timeout} />
+    )
+}
